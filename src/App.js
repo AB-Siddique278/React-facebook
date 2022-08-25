6209d9da856519e11fdd4ea5eae0cc42b704a01a
AB-Siddique278 <absiddique1060@gmail.com>
2022-08-25 12:31:18 +0600

    create a profile section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 
 import './App.css';
 import Home from './pages/home/Home';
+import { Routes, Route, Link } from "react-router-dom";
+import Profile from './pages/profile/Profile';
+
 
 function App() {
   return (
     <div >
-  
-    <Home></Home>
- 
-  
+
+      
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+
+        
+      </Routes>
+
+
+
 
     </div>
   );
--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,44 +1,54 @@
 import React from 'react'
 import "../topbar/topbar.css"
+import { Link } from "react-router-dom";
 import { Search, Person, Chat, Notifications } from "@material-ui/icons";
 export default function Topbar() {
   return (
     <div>
-       <div className="topbarContainer">
-      <div className="topbarLeft">
-        <span className="logo">Facebook</span>
-      </div>
-      <div className="topbarCenter">
-        <div className="searchbar">
-          <Search className="searchIcon" />
-          <input
-            placeholder="Search for friend, post or video"
-            className="searchInput"
-          />
+      <div className="topbarContainer">
+        <div className="topbarLeft">
+
+          <Link to="/">
+            <span className="logo">Facebook</span>
+          </Link>
+
+
         </div>
-      </div>
-      <div className="topbarRight">
-        <div className="topbarLinks">
-          <span className="topbarLink">Homepage</span>
-          <span className="topbarLink">Timeline</span>
-        </div>
-        <div className="topbarIcons">
-          <div className="topbarIconItem">
-            <Person />
-            <span className="topbarIconBadge">1</span>
-          </div>
-          <div className="topbarIconItem">
-            <Chat />
-            <span className="topbarIconBadge">2</span>
-          </div>
-          <div className="topbarIconItem">
-            <Notifications />
-            <span className="topbarIconBadge">1</span>
+        <div className="topbarCenter">
+          <div className="searchbar">
+            <Search className="searchIcon" />
+            <input
+              placeholder="Search for friend, post or video"
+              className="searchInput"
+            />
           </div>
         </div>
-        <img src="/assets/person/1.jpeg" alt="" className="topbarImg"/>
+        <div className="topbarRight">
+          <div className="topbarLinks">
+            <span className="topbarLink">Homepage</span>
+            <span className="topbarLink">Timeline</span>
+          </div>
+          <div className="topbarIcons">
+            <div className="topbarIconItem">
+              <Person />
+              <span className="topbarIconBadge">1</span>
+            </div>
+            <div className="topbarIconItem">
+              <Chat />
+              <span className="topbarIconBadge">2</span>
+            </div>
+            <div className="topbarIconItem">
+              <Notifications />
+              <span className="topbarIconBadge">1</span>
+            </div>
+          </div>
+
+          <Link to="/profile">
+            <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+          </Link>
+
+        </div>
       </div>
-    </div>
     </div>
   )
 }
